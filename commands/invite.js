491e716b74a5149c0f8e7e23c35a000bc9ffe538
JinGamerZ <34589116+JinGamerZ@users.noreply.github.com>
2018-03-02 20:06:42 -0300

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,12 @@
+module.exports = {
+     main: function(bot, msg) {
+		var start = Date.now();
+		var stop, diff;
+		msg.channel.sendMessage("Invite me!").then(function(newMsg) {
+			var stop = Date.now();
+			var diff = (stop - start);
+			newMsg.edit("Invite me! `("+diff+"ms)`");
+		});
+    },
+    help: 'Invitation link'
+};
