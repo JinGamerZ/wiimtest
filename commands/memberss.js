aa928cec335cf58face97d0033137ee232eea9e9
JinGamerZ <34589116+JinGamerZ@users.noreply.github.com>
2018-03-03 06:45:35 -0300

    Delete memberss.js

--- commands/memberss.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-     main: function(bot, msg) {
-		var start = Date.now();
-		var stop, diff;
-    message.channel.send(There are currently ${message.guildmemberCount} Members on this Server!);
-		});
-    },
-    help: 'test'
-};
