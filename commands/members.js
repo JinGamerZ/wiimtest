70bf40bc3efd8c143cc49608af6acc26cec96325
JinGamerZ <34589116+JinGamerZ@users.noreply.github.com>
2018-03-03 06:23:32 -0300

    Create members.js

---
+++ commands/members.js
@@ -0,0 +1,9 @@
+module.exports = {
+  main: (bot, msg, settings) => {
+	//your code goes here
+    this.memberCount = data.member_count || this.memberCount;
+  },
+  args: '<string>',
+  help: 'Members list',
+  hide: false
+}
