054cf650852c07e632a49d1673f7f211864ce72a
JinGamerZ <34589116+JinGamerZ@users.noreply.github.com>
2018-03-03 06:25:56 -0300

    Delete members.js

--- commands/members.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  main: (bot, msg, settings) => {
-	//your code goes here
-    this.memberCount = data.member_count || this.memberCount;
-  },
-  args: '<string>',
-  help: 'Members list',
-  hide: false
-}
