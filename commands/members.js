9bbc74674e1530ff6fee3a6424bba3ad9b17e443
JinGamerZ <34589116+JinGamerZ@users.noreply.github.com>
2018-03-03 06:42:44 -0300

    Delete members.js

--- commands/members.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  main: (bot, msg, settings) => {
-    message.channel.send(There are currently ${message.guildmemberCount} Members on this Server!);
-  },
-  args: '<string>',
-  help: 'List of members',
-  hide: false
-}
