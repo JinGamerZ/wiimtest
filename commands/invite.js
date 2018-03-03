13e740e6bc389da7922eeecd28d66c23fa16d586
JinGamerZ <34589116+JinGamerZ@users.noreply.github.com>
2018-03-02 21:28:14 -0300

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -3,9 +3,6 @@ module.exports = {
 		var start = Date.now();
 		var stop, diff;
 		msg.channel.sendMessage("Invite me!").then(function(newMsg) {
-			var stop = Date.now();
-			var diff = (stop - start);
-			newMsg.edit("Invite me! LINK... ");
 		});
     },
     help: 'Invitation link'
