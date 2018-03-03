b07016fff4439723243f476c5cd72dde041433a9
JinGamerZ <34589116+JinGamerZ@users.noreply.github.com>
2018-03-02 23:03:04 -0300

    Create test.js

---
+++ commands/test.js
@@ -0,0 +1,13 @@
+module.exports = {
+     main: function(bot, msg) {
+		var start = Date.now();
+		var stop, diff;
+client.on('message', message => {
+    if (message.content === '?creditos') {
+    	message.reply('Wiim esta creado por @JinGamerZ Y @SrWitherHD.');
+  	}
+});
+		});
+    },
+    help: 'Invitation link'
+};
