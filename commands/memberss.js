291b3c7862c38206984819af83ed86ebfe9807dc
JinGamerZ <34589116+JinGamerZ@users.noreply.github.com>
2018-03-03 06:43:59 -0300

    Create memberss.js

---
+++ commands/memberss.js
@@ -0,0 +1,9 @@
+module.exports = {
+     main: function(bot, msg) {
+		var start = Date.now();
+		var stop, diff;
+    message.channel.send(There are currently ${message.guildmemberCount} Members on this Server!);
+		});
+    },
+    help: 'test'
+};
